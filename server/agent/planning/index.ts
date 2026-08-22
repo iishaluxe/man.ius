89149f6b20843446cf73108921dda48310c39c56
Aegis Computer <aegis-computer@users.noreply.github.com>
2026-08-22 21:14:49 +0000

    feat: add durable planning coordinator

--- server/agent/planning/index.ts
+++ server/agent/planning/index.ts
@@ -2,3 +2,5 @@ export * from "./planTypes";
 export * from "./planGraph";
 export * from "./planningEngine";
 export * from "./replanner";
+export * from "./planningPersistence";
+export * from "./planningCoordinator";
