68dfcfec687bdda60984ec3fe010fc41c22dfd53
iishaluxe <2.51731043e+08+iishaluxe@users.noreply.github.com>
2026-09-01 17:46:45 +0000

    feat: add intelligence learning ledger

--- server/agent/intelligence/index.ts
+++ server/agent/intelligence/index.ts
@@ -4,7 +4,6 @@ export * from "./modelRegistry";
 export * from "./adaptiveModelRouter";
 export * from "./adaptivePlanningStrategy";
 export * from "./adaptiveIntelligenceGateway";
-
 export * from "./intelligenceEvaluation";
-
 export * from "./intelligenceFeedback";
+export * from "./intelligenceLearning";
